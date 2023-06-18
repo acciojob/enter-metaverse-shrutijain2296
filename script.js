1a9d08ef3cd6ad07502e29e9fee8f1068d0b0235
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-06-18 11:16:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
+document.getElementById("newStatus").style.display = "none";
 document.getElementById("enterBtn").addEventListener('click', (event) =>{
 	event.preventDefault();
 	let status = document.getElementById("status").style.display = "none";
-	let newStatus = document.createElement("h1");
-	newStatus.innerText = "Entered Metaverse";
-	newStatus.style.display = "block";
+	document.getElementById("newStatus").style.display = "block";
+	
 	
 	
 })
